14325a55f6827f083114d4a6c3db6366aede0ec9
owlbunt <owlbunt@gmail.com>
2024-01-12 00:41:46 +0530

    updated

--- productPreviewCard/assets/script.js
+++ productPreviewCard/assets/script.js
@@ -1,7 +1,7 @@
 // Add To Cart Button
 let  addToCartBtn = document.querySelector("button.addToCart");
 addToCartBtn.addEventListener("click", ()=>{
-    addToCartBtn.innerHTML = '<img src="assets/images/icon-check.svg" alt="Cart Icon" class="icon"> Added ';
+    addToCartBtn.innerHTML = '<img src="./assets/images/icon-check.svg" alt="Cart Icon" class="icon"> Added ';
     addToCartBtn.style.background = "#1c232b";
 })
 
