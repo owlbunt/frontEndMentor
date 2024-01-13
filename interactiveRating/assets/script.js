f7f085ae83443e1d27b697139e53b81a93a08169
owlbunt <owlbunt@gmail.com>
2024-01-14 01:46:51 +0530

    updated

--- interactiveRating/assets/script.js
+++ interactiveRating/assets/script.js
@@ -1,6 +1,7 @@
 // Rating Button
 let ratingBtns = document.querySelectorAll(".rating .btn");
 let submitBtn = document.querySelector(".submitBtn");
+let userRating = 0;
 // Add Click function to all 5 buttons 
 ratingBtns.forEach((button) => {
   button.addEventListener("click", () => {
@@ -9,10 +10,22 @@ ratingBtns.forEach((button) => {
       button.classList.remove("active")
     });
     // Add Active Class On Clicked Button 
-    button.classList.add("active");
+    button.classList.add("active"); 
+    // Store the user rating value 
+    userRating =  parseInt(button.innerHTML);
     // Active Sumbit Button with little delay 
     setTimeout(function() {
       submitBtn.classList.add("active")
-    }, 200);
+    }, 100);
   });
 });
+
+// Confirmation Screen
+submitBtn.addEventListener("click", ()=>{
+  if(userRating!= 0){
+    setTimeout(()=>{
+      document.querySelector(".userValue").innerHTML = userRating ;
+      document.querySelector(".confirm").classList.add("active")
+    }, 200)
+  }
+})
