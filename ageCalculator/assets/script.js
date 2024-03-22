eec5bbb154a4e7dedc1a0fe6d94ab61c6fa91a56
owlbunt <owlbunt@gmail.com>
2024-03-22 22:35:09 +0530

    updated

--- ageCalculator/assets/script.js
+++ ageCalculator/assets/script.js
@@ -1,18 +1,30 @@
-let userDateOfBirth = {
-    day : '0',
-    month :'0',
-    year :'0' ,
-}
+// Date Of Birth
+const dob = {
+    year:"00",
+    month :"00",
+    day :"0000",
+};
 
-// Submit Button
+// Calculate Age 
 let submitBtn = document.querySelector(".submitBtn");
 submitBtn.addEventListener("click", ()=>{
-    // Store User DOB
-    userDateOfBirth.day = document.querySelector(".dateInput #day").value ;
-    userDateOfBirth.month = document.querySelector(".dateInput #month").value;
-    userDateOfBirth.year = document.querySelector(".dateInput #year").value;
-});
+  // Get Current Date and Time
+  let newDate = new Date();
+  let currentTime = newDate.getTime();
+  
+  // Get User Date Of Birth
+  dob.year = document.querySelector(".dateInput #year").value;
+  dob.month = document.querySelector(".dateInput #month").value;
+  dob.day = document.querySelector(".dateInput #day").value;
+  
+  // Calculate Age 
+  let dobInMs = new Date(`${dob.year}-${dob.month}-${dob.day}T00:00:00`).getTime();
+  let currentAgeInMs = currentTime - dobInMs;
+  let currentAgeInYear = Math.floor(currentAgeInMs / 1000 / 60 / 60 / 24 / 365.25)
+  console.log(currentAgeInMs)
+  
+  
+  // Update The Age
+  document.querySelector(".totalYears").innerHTML = currentAgeInYear ;
 
-function currentAge(){
-    
-}
+})
